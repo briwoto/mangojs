34040b99775d2d1e483b1eac66f39d5d12a9c8eb
Rahul Singh <50180912+briwoto@users.noreply.github.com>
2019-08-15 01:34:41 +0530

    Update selector.js

--- selector.js
+++ selector.js
@@ -1,4 +1,6 @@
 
-exports.loginusername = 'input[formcontrolname="userName"]';
-exports.loginpassword = 'input[formcontrolname="password"]';
-exports.loginbutton = 'button.ant-btn.ant-btn-ghost';
+module.exports = {
+  'loginusername' = 'input[formcontrolname="userName"]',
+  'loginpassword' = 'input[formcontrolname="password"]',
+  'loginbutton' = 'button.ant-btn.ant-btn-ghost'
+}
