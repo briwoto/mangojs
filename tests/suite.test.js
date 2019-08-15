d03dd9d076c12a6d9206f8a188c6e6c8ee6d0ced
Rahul Singh <50180912+briwoto@users.noreply.github.com>
2019-08-15 17:30:36 +0530

    Update suite.test.js

--- tests/suite.test.js
+++ tests/suite.test.js
@@ -1,5 +1,4 @@
 
-const puppeteer = require('../pre_req').puppeteer;
 const tester = require('../tester');
 
 console.log("started SUITE.js");
@@ -7,13 +6,11 @@ jest.setTimeout(36000000);
 
 describe('testing a portal for any type of case', () => {
   beforeAll(async() => {
-    global.browser = await puppeteer.launch({ "headless": false, args: ['--start-maximized'] });
-    global.page = await browser.newPage();
-    await global.page.setViewport({ width: 0, height: 0 });  
+    await pre_req.ui_setup();
   });
   afterAll(async() => {
-    await browser.close();
-  });
+    await global.driver.quit();
+  });;
   test('testing login', async() => {
     await tester.login.test_login(function(val) {
       expect(val).toBeTruthy;
