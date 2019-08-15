411ce92d28eddcea6356387c96edda97ed61f4a7
Rahul Singh <50180912+briwoto@users.noreply.github.com>
2019-08-15 17:55:05 +0530

    Update pre_req.js

--- pre_req.js
+++ pre_req.js
@@ -1,11 +1,15 @@
 
+var chrome = require('selenium-webdriver/chrome');
+var path = require('chromedriver').path;
+var webdriver = require('selenium-webdriver');
 const sel = require('./selector');
 const _ = require('lodash');
 const ui_util = require('./utils/ui_util');
-const globalVariables = _.pick(global, ['browser', 'page', 'sel', 'ui_util', 'root_dir']);
+const globalVariables = _.pick(global, ['browser', 'page', 'sel', 'ui_util','driver']);
+global.ui_util = ui_util;
+require('dotenv').config();
 global.sel = sel;
 global.root_dir = __dirname;
-require('dotenv').config();
 
 exports.ui_setup = async () => {
   try {
