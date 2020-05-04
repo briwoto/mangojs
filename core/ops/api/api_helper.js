d37845654037dda6332e948b831ef76c6e7c7198
rahul.singh <rahul.beck@gmail.com>
2020-05-05 00:17:36 +0200

    added api_helper.js it contains function to update a payload by fetching the mapped value from map.js, using user's input also, the function to run the service and return the response

---
+++ core/ops/api/api_helper.js
@@ -0,0 +1,36 @@
+const api = require('../../utils/').api_utils,
+	common = require('../../utils/').common_utils;
+
+//This function is used to update the payload with the user data
+//It maps the user input to map.js and adds the mapped_value to the payload
+exports.update_payload = async (payload, page, ar_data, group = '') => {
+	try {
+		for (i in ar_data) {
+			row = ar_data[i];
+			fieldname = group.length ? `${group}_${row[0]}` : row[0];
+			val = row[1];
+			mapped_field = page.fields[fieldname].mapped_to;
+			payload[mapped_field] = val;
+		}
+		return payload;
+	} catch (err) {
+		console.log(`Update payload - EXCEPTION OCCURED:\n${String(err)}`);
+		return null;
+	}
+};
+
+//This function is used to run the webservice and fetch a response
+exports.get_response = async (service) => {
+	try {
+		service.uri = process.env.baseurl_api + service.endpoint;
+		const res = await service.run_service(process.env.ACCESS_TOKEN);
+		if (!res.body) {
+			console.log('Create board failed. No body in response');
+			return null;
+		}
+		return res;
+	} catch (err) {
+		console.log(`Get response - EXCEPTION OCCURED:\n${String(err)}`);
+		return null;
+	}
+};
